d4e42f4b44b62db4f0c5cd0546dcf9ef5095d0b4
Gus Narea <gnarea@users.noreply.github.com>
2020-02-11 12:12:51 +0000

    chore: Actually use make-promises-safe

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// tslint:disable-next-line:no-var-requires
+require('make-promises-safe');
+
 import { commandDir } from 'yargs';
 
 // tslint:disable-next-line:no-unused-expression
