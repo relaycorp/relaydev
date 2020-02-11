6cbee0e4b244d87fa892d8579e09fa5fde574b68
Gus Narea <gnarea@users.noreply.github.com>
2020-02-11 12:03:34 +0000

    chore(test): Add missing link
    To https://github.com/relaycorp/relaynet-core-js/issues/53

--- functional_tests/test-key-genecdh.sh
+++ functional_tests/test-key-genecdh.sh
@@ -2,7 +2,7 @@
 relaydev key gen-ecdh | openssl pkey -in - -inform DER -noout
 
 # Custom named curve:
-# TODO: Enable
+# TODO: Reinstate when https://github.com/relaycorp/relaynet-core-js/issues/53 has been fixed
 #relaydev key gen-ecdh --named-curve=P-521 | \
 #  openssl pkey -in - -inform DER -noout -text | \
 #  grep --quiet 'NIST CURVE: P-521'
