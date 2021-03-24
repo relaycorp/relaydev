15683b89ee7a3a2bd7b878a4b030fa9c1b26d7df
Gus Narea <gnarea@users.noreply.github.com>
2021-03-24 17:37:59 +0000

    chore(CI): Log failing test file to ease debugging

--- functional_tests/run.sh
+++ functional_tests/run.sh
@@ -2,5 +2,9 @@
 
 CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-find "${CURRENT_DIR}" -name 'test-*.sh' -print0 | \
-  xargs -t -0 -n1 bash -x -o nounset -o errexit -o pipefail
+for test_file in find "${CURRENT_DIR}" -name 'test-*.sh' -print0; do
+  if ! bash -x -o nounset -o errexit -o pipefail "${test_file}"; then
+    echo "${test_file} failed." >&2
+    exit 1
+  fi
+done
