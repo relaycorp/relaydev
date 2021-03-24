b42f450e0b658ca14d9daeb117ef07beea71314a
Gus Narea <gnarea@users.noreply.github.com>
2021-03-24 17:44:13 +0000

    use globs

--- functional_tests/run.sh
+++ functional_tests/run.sh
@@ -2,7 +2,7 @@
 
 CURRENT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-for test_file in find "${CURRENT_DIR}" -name 'test-*.sh' -print0; do
+for test_file in "${CURRENT_DIR}"/test-*.sh; do
   if ! bash -x -o nounset -o errexit -o pipefail "${test_file}"; then
     echo "${test_file} failed." >&2
     exit 1
